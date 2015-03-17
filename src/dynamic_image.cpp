2b1c425c1294366181d0930c063ced5f8c720b54
Hans Kirchner <hans-kirchner@gmx.de>
2015-03-17 19:05:45 +0100

    Moved Dynamic/StaticImage here

---
+++ src/dynamic_image.cpp
@@ -0,0 +1,9 @@
+#include <lms/imaging/dynamic_image.h>
+
+namespace lms {
+namespace imaging {
+
+// TODO
+
+} // namespace imaging
+} // namespace lms
---
+++ src/static_image.cpp
@@ -0,0 +1,9 @@
+#include <lms/imaging/static_image.h>
+
+namespace lms {
+namespace imaging {
+
+// TODO
+
+}  // namespace imaging
+}  // namespace lms

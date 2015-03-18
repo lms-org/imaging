b39652ff36aa3ff2ca42f85b81788dd5ed3a13ba
Hans Kirchner <hans-kirchner@gmx.de>
2015-03-18 02:23:48 +0100

    Add Format, fixed cmake stuff

---
+++ include/lms/imaging/format.h
@@ -0,0 +1,46 @@
+#ifndef LMS_IMAGING_FORMAT_H
+#define LMS_IMAGING_FORMAT_H
+
+#include <cstdint>
+#include <iostream>
+
+namespace lms {
+namespace imaging {
+
+enum class Format : std::uint8_t {
+    UNKNOWN = 0, GREY = 1, YUYV = 2
+};
+
+/**
+ * @brief Returns the number of bytes that are needed
+ * to store a single pixel in the given format.
+ *
+ * @param fmt image format
+ * @return -1 if UNKNOWN, 0 if the number of bytes is not integer, a positive value
+ * in all other cases
+ */
+int bytesPerPixel(Format fmt);
+
+/**
+ * @brief Compute the buffer size in bytes that is needed to store
+ * an image with the given parameters.
+ *
+ * @param width width of the image
+ * @param height height of the image
+ * @param fmt image format
+ * @return size in bytes
+ */
+int imageBufferSize(int width, int height, Format fmt);
+
+std::string formatToString(Format fmt);
+
+Format formatFromString(const std::string &string);
+
+std::ostream& operator << (std::ostream &stream, const Format &fmt);
+
+std::ostream& operator >> (std::ostream &stream, Format &fmt);
+
+}  // namespace imaging
+}  // namespace lms
+
+#endif /* LMS_IMAGING_FORMAT_H */
---
+++ src/format.cpp
@@ -0,0 +1,50 @@
+#include "lms/imaging/format.h"
+
+namespace lms {
+namespace imaging {
+
+int bytesPerPixel(Format fmt) {
+    switch(fmt) {
+    case Format::GREY: return 1;
+    case Format::YUYV: return 2;
+    case Format::UNKNOWN:  // go to default
+    default: return -1;  // Don't forget to implement missing formats
+    }
+}
+
+int imageBufferSize(int width, int height, Format fmt) {
+    // TODO do something special if we have strange formats like V4L2_PIX_FMT_Y10BPACK
+    return width * height * bytesPerPixel(fmt);
+}
+
+std::string formatToString(Format fmt) {
+    switch(fmt) {
+    case Format::GREY: return "GREY";
+    case Format::YUYV: return "YUYV";
+    case Format::UNKNOWN:  // go to default
+    default: return "UNKNOWN";
+    }
+}
+
+Format formatFromString(const std::string &str) {
+    if(str == "GREY") return Format::GREY;
+    if(str == "YUYV") return Format::YUYV;
+    return Format::UNKNOWN;
+}
+
+std::ostream& operator << (std::ostream& stream, const Format &fmt) {
+    return stream << formatToString(fmt);
+}
+
+std::istream& operator >> (std::istream &stream, Format &fmt) {
+    std::string str;
+    if(std::getline(stream, str)) {
+        fmt = formatFromString(str);
+    } else {
+        fmt = Format::UNKNOWN;
+    }
+    return stream;
+}
+
+}  // namespace imaging
+}  // namespace lms

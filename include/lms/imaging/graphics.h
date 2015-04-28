6a76652000c107811be19793cb21041c68acbad8
Hans Kirchner <hans-kirchner@gmx.de>
2015-04-28 13:22:22 +0200

    Add class Graphics, BGRAImageGraphics

---
+++ include/lms/imaging/graphics.h
@@ -0,0 +1,107 @@
+#ifndef LMS_IMAGING_GRAPHICS_H
+#define LMS_IMAGING_GRAPHICS_H
+
+#include <vector>
+#include <cstdint>
+
+#include "lms/imaging/image.h"
+
+namespace lms {
+namespace imaging {
+
+struct ARGBColor {
+    ARGBColor(std::uint8_t alpha, std::uint8_t red, std::uint8_t green, std::uint8_t blue);
+    ARGBColor(std::uint8_t red, std::uint8_t green, std::uint8_t blue);
+    ARGBColor(std::uint32_t value);
+    ARGBColor();
+
+    std::uint32_t value;
+};
+
+const ARGBColor black(0, 0, 0);
+const ARGBColor white(255, 255, 255);
+const ARGBColor red(255, 0, 0);
+const ARGBColor green(0, 255, 0);
+const ARGBColor blue(0, 0, 255);
+const ARGBColor yellow(255, 255, 0);
+
+class Graphics {
+public:
+    /**
+     * @brief Set the color that is used for subsequent calls to draw* methods.
+     * @param color RGB color to use (red, green, blue)
+     */
+    virtual void setColor(ARGBColor color) =0;
+
+    /**
+     * @brief Draw a single pixel at (x, y).
+     * @param x coordinate x
+     * @param y coordinate y
+     */
+    virtual void drawPixel(int x, int y) =0;
+
+    /**
+     * @brief Draw a horizontal line from (x, y) to (x+length, y).
+     *
+     * This function is similar to drawLine() but is usually considered to
+     * be faster.
+     *
+     * Negative length values work as expected. A length of zero results in a
+     * single drawPixel(x, y) call.
+     *
+     * @param x coordinate x
+     * @param y coordinate y
+     * @param length length of the line
+     */
+    virtual void drawHorizontalLine(int x, int y, int length);
+
+    /**
+     * @brief Draw a vertical line from (x, y) to (x, y+length).
+     *
+     * This function is similar to drawLine() but is usually considered to
+     * be faster.
+     *
+     * Negative length values work as expected. A length of zero results in a
+     * single drawPixel(x, y) call.
+     *
+     * @param x coordinate x
+     * @param y coordinate y
+     * @param length length of the line
+     */
+    virtual void drawVerticalLine(int x, int y, int length);
+
+    /**
+     * @brief Draw a line from (x1, y1) to (x2, y2).
+     * @param x1 coordinate x of point 1
+     * @param y1 coordinate y of point 1
+     * @param x2 coordinate x of point 2
+     * @param y2 coordinate y of point 2
+     */
+    virtual void drawLine(int x1, int y1, int x2, int y2);
+
+    virtual void drawRect(int x, int y, int width, int height);
+
+    virtual void drawPolyline(int *x, int *y, int n);
+};
+
+class BGRAImageGraphics : public Graphics {
+public:
+    /**
+     * @brief Create a graphics object that is drawing into an image with BGRA
+     * image format.
+     *
+     * @param image image to draw in
+     */
+    BGRAImageGraphics(Image &image);
+
+    void setColor(ARGBColor color) override;
+    void drawPixel(int x, int y) override;
+private:
+    ARGBColor color;
+    Image &image;
+};
+
+}  // namespace imaging
+}  // namespace lms
+
+#endif /* LMS_IMAGING_GRAPHICS_H */
---
+++ src/graphics.cpp
@@ -0,0 +1,80 @@
+#include "lms/imaging/graphics.h"
+#include "lms/imaging/image_factory.h"
+
+namespace lms {
+namespace imaging {
+
+ARGBColor::ARGBColor(std::uint8_t alpha, std::uint8_t red, std::uint8_t green,
+                     std::uint8_t blue) :
+    value(alpha << 24 | red << 16 | green << 8  | blue) {}
+ARGBColor::ARGBColor(std::uint8_t red, std::uint8_t green, std::uint8_t blue) :
+    value(0xff << 24 | red << 16 | green << 8  | blue) {}
+ARGBColor::ARGBColor(std::uint32_t value) : value(value) {}
+ARGBColor::ARGBColor() : value(0x00000000) {}
+
+/**
+ * Helper function that is used to draw horizontal and vertical
+ * lines.
+ */
+template<int dx, int dy>
+inline void drawDeltaLine(Graphics *g, int x, int y, int length) {
+    while(length -- > 0) {
+        g->drawPixel(x, y);
+        x += dx;
+        y += dy;
+    }
+}
+
+void Graphics::drawHorizontalLine(int x, int y, int length) {
+    if(length >= 0) {
+        drawDeltaLine<1, 0>(this, x, y, length);
+    } else {
+        // support negative lengths as well
+        drawDeltaLine<-1, 0>(this, x, y, -length);
+    }
+}
+
+void Graphics::drawVerticalLine(int x, int y, int length) {
+    if(length >= 0) {
+        drawDeltaLine<0, 1>(this, x, y, length);
+    } else {
+        // support negative lengths as well
+        drawDeltaLine<0, -1>(this, x, y, -length);
+    }
+}
+
+void Graphics::drawLine(int x1, int y1, int x2, int y2) {
+    bresenhamLine(x1, y1, x2, y2, [this](int x, int y) {
+        drawPixel(x, y);
+        return true;
+    });
+}
+
+void Graphics::drawRect(int x, int y, int width, int height) {
+    drawHorizontalLine(x, y, width);
+    drawHorizontalLine(x, y + height, width);
+    drawVerticalLine(x, y, height);
+    drawVerticalLine(x + width, y, height);
+}
+
+void Graphics::drawPolyline(int *x, int *y, int n) {
+    for(int i = 0; i < n - 1; i++) {
+        drawLine(x[i], y[i], x[i+1], y[i+1]);
+    }
+}
+
+BGRAImageGraphics::BGRAImageGraphics(Image &image) : image(image) {}
+
+void BGRAImageGraphics::setColor(ARGBColor color) {
+    this->color = color;
+}
+
+void BGRAImageGraphics::drawPixel(int x, int y) {
+    if(x >= 0 && y >= 0 && x < image.width() && y < image.height()) {
+        std::uint32_t *data = reinterpret_cast<std::uint32_t*>(image.data());
+        *(data + x + y * image.width()) = color.value;
+    }
+}
+
+}  // namespace imaging
+}  // namespace lms
